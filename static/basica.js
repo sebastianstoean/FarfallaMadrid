ba78db1cad7dd838c3e9046d217d623dc4e9d2c2
sebastianuc3m <yerthh11@>
2022-02-05 02:04:35 +0100

    mejora

--- static/basica.js
+++ static/basica.js
@@ -64,21 +64,55 @@ function goToPostres() {
 	window.location.href = "carta.html";
 }
 
-function doAjax() {
-	var jqxhr = $.ajax({
-		url: "../static/mail_handler.php",
-		type: "POST",
-		mode: "no-cors",
-		data: {nombre: nombreForm.value,
-					tel: telForm.value,
-					fecha: fechaForm.value,
-					hora: horaForm.value,
-					personas: personasForm.value,
-		},
-		success: function(){
-			alert("successs");
-		}
-	})
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+// function doAjax() {
+// 	var jqxhr = $.ajax({
+// 		url: "../static/mail_handler.php",
+// 		type: "POST",
+// 		mode: "no-cors",
+// 		data: {nombre: nombreForm.value,
+// 					tel: telForm.value,
+// 					fecha: fechaForm.value,
+// 					hora: horaForm.value,
+// 					personas: personasForm.value,
+// 		},
+// 		success: function(){
+// 			alert("successs");
+// 		}
+// 	})
+// }
+language = "es"
+function sendMessage(message) {
+	$.ajax({
+    url:'https://api.telegram.org/bot1832303215:AAFnpjGuRR6mBscYV539yka1LIN8xCjJ25s/sendMessage',
+    method:'POST',
+    data:{chat_id:'985188643',text:message},
+    success:function(){
+  	   alert('your message has been sent!');
+     }
+  });
 }
 
 function noneFlex(a, b) {
@@ -86,10 +120,10 @@ function noneFlex(a, b) {
 	document.getElementById(b+"-"+language).style.display = "none";
 }
 
-function noneNone(a, b, c){
+function noneNone(a, b){
   document.getElementById(a+"-"+language).style.display = "none";
   document.getElementById(b+"-"+language).style.display = "none";
-	c = 1;
+	return 1;
 }
 
 var today = new Date().toISOString().split('T')[0];
@@ -98,24 +132,25 @@ document.getElementsByName("fecha")[0].setAttribute('min', today);
 var e = new Event("look", {"cancelable":true})
 $("#submit1").click(function(e) {
 	e.preventDefault();
-  var nombreForm = (document.getElementById("nombre1"));
-  var telForm = (document.getElementById("tel1"));
-  var fechaForm = (document.getElementById("fecha1"));
-  var horaForm = (document.getElementById("hora1"));
-  var personasForm = (document.getElementById("persona1"));
-  var politicaForm = (document.getElementById("checkbox"));
+  var nombreForm = document.getElementById("nombre1");
+  var telForm = document.getElementById("tel1");
+  var fechaForm = document.getElementById("fecha1");
+  var horaForm = document.getElementById("hora1");
+  var personasForm = document.getElementById("persona1");
+  var politicaForm = document.getElementById("checkbox");
 
-  var $form = document.getElementById('formreservar');
+  var $form = $('#formreservar');
 
   let reName = new RegExp("^[a-zA-Z ]{2,30}$");
   let reTel = new RegExp("^[0-9]{0,18}$");
+  var arrayMatches = nombreForm.value.match(reName);
+
   var nombre = 0;
   var tel = 0;
   var fecha = 0;
   var hora = 0;
   var check = 0;
 
-	var arrayMatches = nombreForm.value.match(reName);
   if (nombreForm.value == "") {
 		noneFlex("no-nombre", "mal-nombre");
   }
@@ -123,7 +158,7 @@ $("#submit1").click(function(e) {
 		noneFlex("mal-nombre", "no-nombre");
   }
   else {
-		noneNone("no-nombre", "mal-nombre", nombre);
+		nombre = noneNone("no-nombre", "mal-nombre");
   }
 
   if (telForm.value == "") {
@@ -133,35 +168,35 @@ $("#submit1").click(function(e) {
 		noneFlex("mal-tel", "no-tel");
   }
   else {
-		noneNone("no-tel", "mal-tel", tel);
+		tel = noneNone("no-tel", "mal-tel");
   }
 
   if (fechaForm.value == ""){
-    document.getElementById("no-fecha-"+language).style.display = "flex";
+    $("#no-fecha-"+language).attr("display", "flex");
   }
   else {
-    document.getElementById("no-fecha-"+language).style.display = "none";
+    $("#no-fecha-"+language).attr("display", "none");
     fecha = 1;
 
     if (horaForm.value == ""){
 			noneFlex("no-hora", "mal-hora");
     }
     else {
-      var hora = horaForm.value[0];
-      hora += horaForm.value[1];
+      var horaComprobar = horaForm.value[0];
+      horaComprobar += horaForm.value[1];
       var min = horaForm.value[3];
       min += horaForm.value[4];
       var dia = fechaForm.valueAsDate.toString()[0];
       dia += fechaForm.valueAsDate.toString()[1];
       dia += fechaForm.valueAsDate.toString()[2];
-      var horaNum = Number(hora);
+      var horaNum = Number(horaComprobar);
       var minNum = Number(min);
       if (dia == "Fri" || dia == "Sat" || dia == "Sun") {
-        if ((hora > 1 && hora < 13 || hora == 1 && min > 45)) {
+        if ((horaComprobar > 1 && horaComprobar < 13 || horaComprobar == 1 && min > 45)) {
 				 	noneFlex("mal-hora", "no-hora");
         }
         else {
-					noneNone("no-hora", "mal-hora", hora);
+					hora = noneNone("no-hora", "mal-hora");
         }
       }
       else {
@@ -171,21 +206,23 @@ $("#submit1").click(function(e) {
 					noneFlex("mal-hora", "no-hora");
         }
         else {
-          noneNone("no-hora", "mal-hora", hora);
+          hora = noneNone("no-hora", "mal-hora");
         }
       }
     }
   }
 
   if (!politicaForm.checked) {
-    document.getElementById("no-politica-"+language).style.display = "flex";
+    $("#no-politica-"+language).attr("display", "flex");
   }
   else {
-    document.getElementById("no-politica-"+language).style.display = "none";
+    $("#no-politica-"+language).attr("display","none");
     check = 1;
   }
 
   if (nombre == 1 && tel == 1 && fecha == 1 && hora == 1 && check == 1) {
-    doAjax();
+    var message = 'Hay una reserva para '+personasForm.value+' personas el día '
+    +fechaForm.value+' a las '+horaForm.value+' \nA nombre de '+nombreForm.value+' con teléfono '+telForm.value;
+    sendMessage(message);
   }
 })
