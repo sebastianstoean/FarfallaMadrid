7546d84275b9de5868b2af9b178e3497bdcb62b9
sebastianuc3m <yerthh11@>
2022-03-06 23:39:54 +0100

    bienvenido

--- static/basica.js
+++ static/basica.js
@@ -2,6 +2,9 @@ console.log(screen.height, screen.width);
 let language = "es";
 var e = new Event("look", {"cancelable":true})
 let idiomas_on = 0
+let lookat = ""
+let cookie = "0"
+let publicKey = "MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgHVeaoWG8j2U2sp3azfqtLQ54P6R+w2dBZ/3RIll8srgOhPMwqSkTVIKzaM79hrtzZEybLshiIyN37rlae0lSoWaLlsZWCq6wQwiBARJabLHLwh0hIt2q1ku5+WmvicwzSVA3pDagwFq4JewyqqbZJ+ksmD1mH7LUGwV6tbmyaKPAgMBAAE="
 
 function sleepFor(sleepDuration){
     var now = new Date().getTime();
@@ -38,8 +41,6 @@ function hide_unhide_dropdown(){
 }
 
 $(document).ready(function() {
-
-
   $("#espanol-boton").click(function(){
     if(language !== "es") {
       language = languageChange(language, "es");
@@ -74,13 +75,28 @@ $(document).ready(function() {
   $("#carta-boton").click(function() {
     goToCarta()
   });
+
+  $("#reservar-boton").click(function() {
+    	var reservas = document.getElementById("reserva-sect");
+    	reservas.scrollIntoView({behavior: 'smooth' });
+  })
+
+  $(".popup-x").click(function(){
+    $(this).closest(".popup").hide()
+  })
+
+  $(".cookie-accept").click(function(){
+    $(this).closest(".cookie").hide()
+    setCookie("accepted", "1", 730)
+  })
+
+  cookie = getCookie("accepted")
+  if (cookie === "1") {
+    $(".cookie").hide()
+  } else {
+    $(".cookie").css("display", "flex")
+  }
 })
-
-function goToReservar(){
-	var reservas = document.getElementById("reserva-sect");
-	reservas.scrollIntoView({behavior: 'smooth' });
-}
-
 function goToCarta(){
 	window.location.href = "../templates/cartas.html";
 }
@@ -91,33 +107,39 @@ function goToSinGluten() {
 
 function goToEntrantes() {
   window.location.href = "../templates/cartas.html";
-  window.addEventListener("load", function(){
-    console.log("a")
-    $("#boton-entrantes").click()
-  })
+  lookat = "boton-entrantes"
+  setCookie("lookat", lookat, 1)
 }
 
 function goToPastas() {
-	window.location.href = "carta.html";
+	window.location.href = "../templates/cartas.html";
+  lookat = "boton-veg"
+  setCookie("lookat", lookat, 1)
 }
 
 function goToPizzas() {
-	window.location.href = "carta.html";
+	window.location.href = "../templates/cartas.html";
+  lookat = "boton-pizz"
+  setCookie("lookat", lookat, 1)
 }
 
 function goToCarnes() {
-	window.location.href = "carta.html";
+	window.location.href = "../templates/cartas.html";
+  lookat = "boton-carnes"
+  setCookie("lookat", lookat, 1)
 }
 
 function goToPostres() {
-	window.location.href = "carta.html";
+	window.location.href = "../templates/cartas.html";
+  lookat = "boton-postres"
+  setCookie("lookat", lookat, 1)
 }
 
 function setCookie(cname, cvalue, exdays) {
   const d = new Date();
   d.setTime(d.getTime() + (exdays*24*60*60*1000));
   let expires = "expires="+ d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";SameSite=Lax" + ";path=/";
 }
 
 function getCookie(cname) {
@@ -152,15 +174,30 @@ function getCookie(cname) {
 // 		}
 // 	})
 // }
-function sendMessage(message) {
-	$.ajax({
-    url:'https://api.telegram.org/bot1832303215:AAFnpjGuRR6mBscYV539yka1LIN8xCjJ25s/sendMessage',
-    method:'POST',
-    data:{chat_id:'985188643',text:message},
-    success:function(){
-  	   alert('your message has been sent!');
-     }
-  });
+function sendMessage(personas, fecha, hora, nombre, tel) {
+  try  {
+    let RSAEncrypt = new JSEncrypt();
+    RSAEncrypt.setPublicKey(publicKey);
+    var message = 'Hay una reserva para '+personas+' personas el día '+fecha+' a las '+hora+' \nA nombre de '+nombre+' con teléfono: '+tel;
+    message = RSAEncrypt.encrypt(message);
+
+    $.ajax({
+      url:'https://api.telegram.org/bot1832303215:AAFnpjGuRR6mBscYV539yka1LIN8xCjJ25s/sendMessage',
+      method:'POST',
+      data:{chat_id:'985188643',text:message},
+      success:function(){
+        $("#reserva-texto-popup").html("Reserva realizada correctamente para el "+fecha+" a las "+hora+" teléfono: "+tel+"<br><br>Si esta información no es correcta llámenos al 913 69 43 91.")
+      },
+      fail:function(){
+        $("#reserva-texto-popup").html("No se ha podido realizar la reserva, si esto sigue ocurriendo puede llamarnos al 913 69 43 91")
+      }
+    });
+  } catch(error) {
+    $("#reserva-texto-popup").html("No se ha podido realizar la reserva, si esto sigue ocurriendo puede llamarnos al 913 69 43 91")
+  }
+
+  sleepFor(1)
+  $("#reservaHecha").css("display", "flex")
 }
 
 function noneFlex(a, b) {
@@ -205,6 +242,7 @@ $("#submit1").click(function(e) {
   }
   else {
 		nombre = noneNone("no-nombre", "mal-nombre");
+    nombre = 1
   }
 
   if (telForm.value == "") {
@@ -215,13 +253,14 @@ $("#submit1").click(function(e) {
   }
   else {
 		tel = noneNone("no-tel", "mal-tel");
+    tel = 1
   }
 
   if (fechaForm.value == ""){
-    $("#no-fecha-"+language).attr("display", "flex");
+    $("#no-fecha-"+language).css("display", "flex");
   }
   else {
-    $("#no-fecha-"+language).attr("display", "none");
+    $("#no-fecha-"+language).css("display", "none");
     fecha = 1;
 
     if (horaForm.value == ""){
@@ -237,22 +276,33 @@ $("#submit1").click(function(e) {
       dia += fechaForm.valueAsDate.toString()[2];
       var horaNum = Number(horaComprobar);
       var minNum = Number(min);
-      if (dia == "Fri" || dia == "Sat" || dia == "Sun") {
-        if ((horaComprobar > 1 && horaComprobar < 13 || horaComprobar == 1 && min > 45)) {
+      if (dia == "Fri" || dia == "Sat") {
+        if ((horaNum >= 1 && horaNum < 13) || (horaNum == 0 && minNum > 30)) {
 				 	noneFlex("mal-hora", "no-hora");
         }
         else {
 					hora = noneNone("no-hora", "mal-hora");
+          hora = 1
+        }
+      }
+      else if (dia == "Sun") {
+        if ((horaNum > 15 || horaNum < 13) || (horaNum == 15 && minNum > 30)) {
+          noneFlex("mal-hora", "no-hora");
+        }
+        else {
+          hora = noneNone("no-hora", "mal-hora");
+          hora = 1
         }
       }
       else {
-        if ((horaNum > 1 && horaNum < 13) || (horaNum == 1 && minNum > 45) ||
-        (horaNum > 15 && horaNum < 19) || (horaNum == 15 && minNum > 45) ||
+        if ((horaNum > 1 && horaNum < 13) || (horaNum == 23) || (horaNum == 22 && minNum > 01) ||
+        (horaNum > 15 && horaNum < 19) || (horaNum == 15 && minNum > 30) ||
         (horaNum == 19 && minNum < 30) ) {
 					noneFlex("mal-hora", "no-hora");
         }
         else {
           hora = noneNone("no-hora", "mal-hora");
+          hora = 1
         }
       }
     }
@@ -266,9 +316,7 @@ $("#submit1").click(function(e) {
     check = 1;
   }
 
-  if (nombre == 1 && tel == 1 && fecha == 1 && hora == 1 && check == 1) {
-    var message = 'Hay una reserva para '+personasForm.value+' personas el día '
-    +fechaForm.value+' a las '+horaForm.value+' \nA nombre de '+nombreForm.value+' con teléfono '+telForm.value;
-    sendMessage(message);
+  if (nombre === 1 && tel === 1 && fecha === 1 && hora === 1 && check === 1) {
+    sendMessage(personasForm.value, fechaForm.value, horaForm.value, nombreForm.value, telForm.value);
   }
 })
--- static/cartas.js
+++ static/cartas.js
@@ -9,4 +9,37 @@ $(document).ready(function(){
       texts.slideUp("slow");
     }
   })
+
+  lookat = getCookie("lookat")
+
+  if (lookat !== "") {
+    document.getElementById(lookat).scrollIntoView({ behavior: "smooth" });
+    $('#'+lookat).click()
+    setCookie("lookat", "", 1)
+  }
 })
+
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";SameSite=Lax" + ";path=/";
+}
